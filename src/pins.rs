3c6c4a26b0b9279953c3d84ad7211bebc27c2ec9
Spencer Christiansen <jc.spencer92@gmail.com>
2021-04-30 10:35:38 -0600

    Test pins

--- src/pins.rs
+++ src/pins.rs
@@ -1,5 +1,8 @@
 use super::geometry::Point;
 use rand::RngCore;
+use std::collections::HashSet;
+
+const P: fn(u32, u32) -> Point = Point::new;
 
 pub fn generate(pin_arrangement: &str, desired_count: u32, width: u32, height: u32) -> Vec<Point> {
     let generator = match pin_arrangement {
@@ -14,26 +17,22 @@ pub fn generate(pin_arrangement: &str, desired_count: u32, width: u32, height: u
 }
 
 fn perimeter(desired_count: u32, width: u32, height: u32) -> Vec<Point> {
-    let desired_count = u32::max(4, desired_count);
-    let spacingf = f64::max(
-        1.0,
-        ((width + height - 2) * 2) as f64 / desired_count as f64,
-    );
-    let countf = ((width + height - 2) * 2) as f64 / spacingf;
+    let perimeter_pixels = (width + height - 2) * 2;
+    let spacing = f64::max(1.0, perimeter_pixels as f64 / desired_count as f64);
+    let count = perimeter_pixels as f64 / spacing;
     let ratio = width as f64 / height as f64;
-    let h_countf = countf / 2.0 * ratio / (1.0 + ratio);
-    let v_countf = countf / 2.0 - h_countf;
+    let h_count = count / 2.0 * ratio / (1.0 + ratio);
+    let v_count = count / 2.0 - h_count;
 
-    let horizontal_count = h_countf.round() as u32;
-    let vertical_count = v_countf.round() as u32;
-    let h_spacingf = (width as f64) / (horizontal_count as f64);
-    let v_spacingf = (height as f64) / (vertical_count as f64);
+    let h_count = h_count.round() as u32;
+    let v_count = v_count.round() as u32;
+    let h_spacing = (width as f64) / (h_count as f64);
+    let v_spacing = (height as f64) / (v_count as f64);
 
-    let top = (0..horizontal_count).map(|i| Point::new(f_mul(i, h_spacingf), 0));
-    let bottom =
-        (0..horizontal_count).map(|i| Point::new(width - f_mul(i, h_spacingf) - 1, height - 1));
-    let left = (0..vertical_count).map(|i| Point::new(0, height - f_mul(i, v_spacingf) - 1));
-    let right = (0..vertical_count).map(|i| Point::new(width - 1, f_mul(i, v_spacingf)));
+    let top = (0..h_count).map(|i| P(f_mul(i, h_spacing), 0));
+    let bottom = (0..h_count).map(|i| P(width - f_mul(i, h_spacing) - 1, height - 1));
+    let left = (0..v_count).map(|i| P(0, height - f_mul(i, v_spacing) - 1));
+    let right = (0..v_count).map(|i| P(width - 1, f_mul(i, v_spacing)));
 
     top.chain(right).chain(bottom).chain(left).collect()
 }
@@ -43,32 +42,28 @@ fn f_mul(i: u32, f: f64) -> u32 {
 }
 
 fn grid(desired_count: u32, width: u32, height: u32) -> Vec<Point> {
-    let r = width as f64 / height as f64;
-    let x = (desired_count as f64 * r).sqrt().round() as usize;
-    let y = (desired_count as f64 / r).sqrt().round() as usize;
-    let dx = width as f64 / x as f64;
-    let dy = height as f64 / y as f64;
-    let max_x = width - 1;
-    let max_y = height - 1;
+    let ratio = width as f64 / height as f64;
+    let x = u32::min(width, (desired_count as f64 * ratio).sqrt().round() as u32);
+    let y = u32::min(height, (desired_count as f64 / ratio).sqrt().round() as u32);
+    let dx = (width - 1) as f64 / (u32::max(x, 1) - 1) as f64;
+    let dy = (height - 1) as f64 / (u32::max(y, 1) - 1) as f64;
 
-    (0..=y)
-        .flat_map(|j| {
-            (0..=x).map(move |i| {
-                Point::new(
-                    u32::min(max_x, (i as f64 * dx) as u32),
-                    u32::min(max_y, (j as f64 * dy) as u32),
-                )
-            })
-        })
+    (0..y)
+        .flat_map(|j| (0..x).map(move |i| P(f_mul(i, dx), f_mul(j, dy))))
         .collect()
 }
 
 fn random(desired_count: u32, width: u32, height: u32) -> Vec<Point> {
+    let desired_count = u32::min(width * height, desired_count);
+    let mut points = HashSet::new();
     let mut rng = rand::thread_rng();
-    (0..desired_count)
-        .map(|_| (rng.next_u32() % width, rng.next_u32() % height))
-        .map(|(x, y)| Point::new(x, y))
-        .collect()
+    loop {
+        if points.len() == desired_count as usize {
+            return points.into_iter().collect();
+        } else {
+            points.insert(P(rng.next_u32() % width, rng.next_u32() % height));
+        }
+    }
 }
 
 fn circle(desired_count: u32, width: u32, height: u32) -> Vec<Point> {
@@ -78,11 +73,13 @@ fn circle(desired_count: u32, width: u32, height: u32) -> Vec<Point> {
     let step_size = std::f64::consts::PI * 2.0 / desired_count as f64;
     (0..desired_count)
         .map(|step| {
-            Point::new(
+            P(
                 ((radius * (step as f64 * step_size).cos()).round() + center_x) as u32,
                 ((radius * (step as f64 * step_size).sin()).round() + center_y) as u32,
             )
         })
+        .collect::<HashSet<_>>() // remove duplicates
+        .into_iter()
         .collect()
 }
 
@@ -91,19 +88,55 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_specifying_too_few_pins_returns_minimum() {
+    fn test_perimeter_specifying_0_points_works() {
         let pins = perimeter(0, 1234, 1234);
-        assert_eq!(4, pins.len())
+        assert_eq!(0, pins.len())
     }
 
     #[test]
-    fn test_specifying_too_many_pins_returns_maximum() {
+    fn test_grid_specifying_0_points_works() {
+        let pins = grid(0, 1234, 1234);
+        assert_eq!(0, pins.len())
+    }
+
+    #[test]
+    fn test_random_specifying_0_points_works() {
+        let pins = random(0, 1234, 1234);
+        assert_eq!(0, pins.len())
+    }
+
+    #[test]
+    fn test_circle_specifying_0_points_works() {
+        let pins = circle(0, 1234, 1234);
+        assert_eq!(0, pins.len())
+    }
+
+    #[test]
+    fn test_perimeter_specifying_too_many_pins_returns_maximum() {
         let pins = perimeter(60, 10, 10);
         assert_eq!(36, pins.len())
     }
 
     #[test]
-    fn test_generate_pins() {
+    fn test_grid_specifying_too_many_pins_returns_maximum() {
+        let pins = grid(600, 10, 10);
+        assert_eq!(100, pins.len())
+    }
+
+    #[test]
+    fn test_random_specifying_too_many_pins_returns_maximum() {
+        let pins = random(600, 10, 10);
+        assert_eq!(100, pins.len())
+    }
+
+    #[test]
+    fn test_circle_specifying_too_many_pins_returns_maximum() {
+        let pins = circle(600, 10, 10);
+        assert_eq!(34, pins.len())
+    }
+
+    #[test]
+    fn test_perimeter_generate_pins_count() {
         for count in [4, 8, 16, 60, 120, 200, 400, 1000].iter() {
             for (width, height) in [(123, 457), (2880, 1800), (1234, 5678), (10, 10000)].iter() {
                 let pins = perimeter(*count, *width, *height);
@@ -117,5 +150,40 @@ mod test {
                 );
             }
         }
+    }
+
+    #[test]
+    fn test_perimeter_generate_pins_locations() {
+        assert_eq!(
+            vec![
+                P(0, 0),
+                P(12, 0),
+                P(24, 0),
+                P(24, 12),
+                P(24, 24),
+                P(12, 24),
+                P(0, 24),
+                P(0, 12)
+            ],
+            perimeter(8, 25, 25)
+        )
+    }
+
+    #[test]
+    fn test_grid_generate_pins_locations() {
+        assert_eq!(
+            vec![
+                P(0, 0),
+                P(12, 0),
+                P(24, 0),
+                P(0, 12),
+                P(12, 12),
+                P(24, 12),
+                P(0, 24),
+                P(12, 24),
+                P(24, 24),
+            ],
+            grid(9, 25, 25)
+        )
     }
 }
