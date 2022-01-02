4d8fe69985f2ea6b22f905b1d8434434247b9ca8
Spencer Christiansen <jc.spencer92@gmail.com>
2022-01-01 23:28:06 -0700

    Include total string distance in pretty_instructions

--- scripts/pretty_instructions.rb
+++ scripts/pretty_instructions.rb
@@ -4,6 +4,10 @@ require 'json'
 
 file_name = ARGV[0]
 
+INCH_WIDTH = ARGV[1].to_f
+
+raise "Width argument must be a positive float!" unless INCH_WIDTH.positive?
+
 json = JSON.parse(File.read(file_name))
 
 pin_arrangement = json.dig("args", "pin_arrangement")
@@ -19,18 +23,21 @@ PIN_COUNT = json.dig("args", "pin_count")
 raise "Expected pin count to be an integer." unless PIN_COUNT.is_a?(Integer)
 raise "Expected pin count to be positive" unless PIN_COUNT.positive?
 
-pin_locations = json.fetch("pin_locations")
+PIN_LOCATIONS = json.fetch("pin_locations")
 
-raise "Expected pin locations to be an array." unless pin_locations.is_a?(Array)
+raise "Expected pin locations to be an array." unless PIN_LOCATIONS.is_a?(Array)
 
 line_segments = json.fetch("line_segments")
 
 raise "Expected line segments to be an array." unless line_segments.is_a?(Array)
 
+INCH_PER_PIXEL = INCH_WIDTH / json.fetch("image_width")
+PIN_SPACING_INCH = Math::PI * INCH_WIDTH / PIN_LOCATIONS.count.to_f
+
 numbered_line_segments = line_segments.map do |ls|
   [
-    pin_locations.index(ls.fetch(0)) || raise("pin not found! #{ls[0]}"),
-    pin_locations.index(ls.fetch(1)) || raise("pin not found! #{ls[1]}"),
+    PIN_LOCATIONS.index(ls.fetch(0)) || raise("pin not found! #{ls[0]}"),
+    PIN_LOCATIONS.index(ls.fetch(1)) || raise("pin not found! #{ls[1]}"),
   ].sort
 end
 
@@ -108,12 +115,25 @@ def num_to_padded_letters(num)
   ([LETTER_BASE.first] * (DIGITS - a.count) + a).join(' ')
 end
 
+def segment_inch_dist(ia, ib)
+  a = PIN_LOCATIONS[ia]
+  b = PIN_LOCATIONS[ib]
+  ((a['x'] - b['x'])**2 + (a['y'] - b['y'])**2)**0.5 * INCH_PER_PIXEL
+end
+
 total = ordered_segments.count
 last_point = ordered_segments.first.first
+string_length = 0
 ordered_segments.each_with_index do |s, i|
   puts if (i % BASE).zero?
   puts "You have strung #{i} strings (there are #{total - i} left).\n" if (i % BASE**2).zero?
-  puts "(Around from pin '#{last_point}' to pin '#{s[0]}')" if last_point != s[0]
+  if last_point != s[0]
+    puts "(Around from pin '#{last_point}' to pin '#{s[0]}')" if last_point != s[0]
+    string_length += point_to_point_dist(last_point, s[0]) * PIN_SPACING_INCH
+  end
   puts "[#{num_to_padded_letters(i)}] From pin '#{s[0]}' to pin '#{s[1]}'."
+  string_length += segment_inch_dist(s[0], s[1])
   last_point = s[1]
 end
+
+puts "\nTotal string distance: #{string_length.round}in or #{(string_length / 39_370.1).round(3)}km"
